78000a12b984e3e1a19c388e750d6430ba01fb81
Robert Varela <robert.varela@rvarela-ret.local>
2017-04-27 18:25:27 -0500

    added nested routing

--- src/components/Page1-user.js
+++ src/components/Page1-user.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-class Page1 extends Component {
+class Page1User extends Component {
 
   render () {
     return <div>
-      <h1>Page 1 </h1>
+      <h1> Page 1 User</h1>
       <Link to='/'> Home </Link>
-      <Link to='/page1/user'> Page 1 User </Link>
+      <Link to='/page1'> Page 1 </Link>
     </div>
   }
 }
 
-export default Page1;
+export default Page1User;
